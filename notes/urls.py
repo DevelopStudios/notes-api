27796cf1b7567934560d3422e0d77026c10bf1db
Charl <Charlroux641@gmail.com>
2025-07-29 12:56:32 +0000

    added ulr page

---
+++ notes/urls.py
@@ -0,0 +1,16 @@
+# notes/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import NoteViewSet, TagViewSet
+
+# Create a router instance
+router = DefaultRouter()
+
+# Register your ViewSets with the router
+router.register(r'notes', NoteViewSet)
+router.register(r'tags', TagViewSet)
+
+# Define the urlpatterns for the 'notes' app
+urlpatterns = [
+    path('', include(router.urls)),
+]
